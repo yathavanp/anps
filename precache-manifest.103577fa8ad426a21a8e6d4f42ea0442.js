57ce0f7bf7e82a71b4709da5601e33affb09118c
Yathavan Push <pushyathavan@gmail.com>
2020-07-27 12:26:27 -0400

    Updates

---
+++ precache-manifest.103577fa8ad426a21a8e6d4f42ea0442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ac338aaba32ae03e012399f1d121a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896324b4b1233ebba279",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a3168a245c7cd67ccc0",
+    "url": "/static/js/2.911e9cd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.911e9cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896324b4b1233ebba279",
+    "url": "/static/js/main.11bd9384.chunk.js"
+  },
+  {
+    "revision": "a9aa74df4fe0120a1785",
+    "url": "/static/js/runtime-main.89d21b56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
